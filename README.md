88f98bb70992032138e15f69a0eab9b5792609ed
Vera Mazhuga <vmazhuga@tappsi.co>
2017-02-17 14:59:41 -0500

    Initial file structure

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # python-bot-utils
+
+## Installation
+
+```
+pip install git+ssh://git@github.com/vero4karu/python-bot-utils.git@v0.1
+```
+
+## Facebook messenger
+
+## Telegram
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='python_bot_utils',
+    version='0.0.1',
+    author='vero4ka',
+    packages=['python_bot_utils'],
+    description='Helper methods to build python bots.',
+    long_description=open('README.md').read(),
+)
